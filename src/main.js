ee883f59826b3259aaeb7eefc2b0929f419a174a
htikkke <htikew574@gmail.com>
2026-06-04 14:15:37 +0630

    add js for menubar.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const menuIcon = document.getElementById("menu-icon");
+const popupMenu = document.getElementById("popup-menu");
+menuIcon.addEventListener("click", (e) => {
+  popupMenu.classList.remove("hidden");
+  popupMenu.classList.add("flex");
+
+  e.stopPropagation();
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    !popupMenu.classList.contains("hidden") &&
+    !popupMenu.contains(e.target)
+  ) {
+    popupMenu.classList.remove("flex");
+    popupMenu.classList.add("hidden");
+  }
+});
